d7e82de6fafc997db7e6f46624ed9623ac230461
Christie Maduna <ferggiee@gmail.com>
2021-11-05 16:25:10 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,17 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: "Fira Sans", "sans-serif";
-
+  font-family: "Fira Sans", sans-serif;
   &::selection {
     background: transparentize(#42b883, 0.5);
   }
 }
-
 body {
   background-color: #35495e;
 }
-
 a {
   text-decoration: none;
 }
-
 header {
   display: flex;
   align-items: center;
@@ -36,11 +32,9 @@ header {
   padding: 10px 16px;
   background-color: #2c3d4e;
   box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
-
   h1 {
     color: #fff;
     font-size: 28px;
-
     span {
       color: #42b883;
     }
